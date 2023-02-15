26d91f05eaf76893384c6ddb7bd9a6ed0d71a724
Saad T <tariqs26@mcmaster.ca>
2023-02-14 20:12:39 -0500

    add edge case for value of 0

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -3,19 +3,23 @@ import styles from './Node.module.css';
 
 type Props = {
   value?: number;
-  next?: number | null;
 };
 
 export default function Node({ value }: Props) {
+  const isNumber = typeof value === 'number';
   return (
     <motion.div
-      className={styles.node}
+      className={styles.node_container}
       layout
       initial={{ y: -50, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       exit={{ y: -50, opacity: 0 }}
     >
-      {value ?? 'null'}
+      <p className={styles.node}>
+        <span>{value ?? 'Null'}</span>
+        {isNumber && <span className={styles.spacer} />}
+      </p>
+      {isNumber && <div className={styles.arrow} />}
     </motion.div>
   );
 }
